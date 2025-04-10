bdfae11b999cfbe318fc26a830c6a699dcccbd70
NepNath <neptonath@gmail.com>
2025-04-10 14:46:27 +0200

    continued dash with alternative method

--- Assets/Scripts/New Rigid Body/Dash.cs
+++ Assets/Scripts/New Rigid Body/Dash.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using Unity.VisualScripting;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class Dash : MonoBehaviour
 {
 
+    [Header("References")]
     public Movement Movement;
     public Rigidbody Rigidbody;
     public float DashForce;
     Vector3 PlayerInput;
+
+    [Header("dash")]
     public float DashCooldown;
+    public float dashDuration;
     private bool canDash;
+    private bool isDashing;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,20 +32,34 @@ public class Dash : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.LeftShift))
         {
-            StartCoroutine(dash());
+            StartCoroutine(dashForce());
         }
         resetDash();
     }
 
 
-    IEnumerator dash()
+    IEnumerator dashForce()
     {
         Rigidbody.AddForce(PlayerInput  * DashForce, ForceMode.Impulse);
         yield return new WaitForSeconds(DashCooldown);
     }
 
+    IEnumerator dashTime()
+    {
+        canDash = false;
+        isDashing = true;
+        float Timer = Time.time;
+
+        while (Time.time < Timer + dashDuration)
+        {
+            Rigidbody.linearVelocity = new Vector3()
+        }
+
+    }
+
     void resetDash()
     {
         canDash = true;
+        isDashing = false;
     }
 }
