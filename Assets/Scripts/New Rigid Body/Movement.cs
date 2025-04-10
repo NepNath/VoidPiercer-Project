ef07fac53e1cbf8c4e3330a45c4d6ec9e94e941d
NepNath <neptonath@gmail.com>
2025-04-10 12:50:12 +0200

    fixed wall grip & added jump raycast

--- Assets/Scripts/New Rigid Body/Movement.cs
+++ Assets/Scripts/New Rigid Body/Movement.cs
@@ -5,25 +5,41 @@ public class Movement : MonoBehaviour
     private Vector3 PlayerInput;
 
     public float speed;
+    public float maxSpeed;
     public float JumpForce;
     public Rigidbody Rigidbody;
+    public float GroundRayLength;
 
     private void Update()
     {
         PlayerInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
         PlayerMove();
+
+        Debug.DrawRay(transform.position, Vector3.down * GroundRayLength, Color.green);
+
+        if(IsRayGrounded())
+        {
+            Debug.Log("Is Ray grounded");
+        }
     }
 
     private void PlayerMove()
     {
         Vector3 MoveVector = transform.TransformDirection(PlayerInput) * speed;
-        Vector3 clampedMoveVector = Vector3.ClampMagnitude(MoveVector, speed);
+        Vector3 clampedMoveVector = Vector3.ClampMagnitude(MoveVector, maxSpeed);
         Rigidbody.linearVelocity = new Vector3(clampedMoveVector.x, Rigidbody.linearVelocity.y, clampedMoveVector.z);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && IsRayGrounded())
         {
             Rigidbody.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
         }
     }
+
+    private bool IsRayGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, GroundRayLength);
+    }
+
+    
 
 }
